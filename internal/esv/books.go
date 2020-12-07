86a25e594cc5547bfcb38a18f5792cab93e91076
Sterling Hanenkamp <sterling@hanenkamp.com>
2020-12-07 09:43:00 -0600

    capture possible book/chapter/verse refs
    I found a list of these online, and then I convert the data into Golang
    code so I don't need to worry about parsing this constant data out.

---
+++ internal/esv/books.go
@@ -0,0 +1,33 @@
+package esv
+
+import (
+	"strconv"
+)
+
+//go:generate go run ../../scripts/gen/verses/main.go
+
+type Book struct {
+	name   string
+	verses []VerseRef
+}
+
+type VerseRef interface {
+	Ref() string
+}
+
+type ChapterVerse struct {
+	chapter int
+	verse   int
+}
+
+func (v *ChapterVerse) Ref() string {
+	return strconv.Itoa(v.chapter) + ":" + strconv.Itoa(v.verse)
+}
+
+type JustVerse struct {
+	verse int
+}
+
+func (v *JustVerse) Ref() string {
+	return strconv.Itoa(v.verse)
+}
