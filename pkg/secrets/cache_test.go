04e01faad118e7e7165009371cbfe767627e42af
Sterling Hanenkamp <sterling@hanenkamp.com>
2021-05-21 09:14:48 -0500

    adding better testing of the secret cacher

--- pkg/secrets/cache_test.go
+++ pkg/secrets/cache_test.go
@@ -3,6 +3,9 @@ package secrets
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCacher(t *testing.T) {
@@ -21,4 +24,34 @@ func TestCacher(t *testing.T) {
 	}
 
 	SecretKeeperTestSuite(t, factory)
+}
+
+func TestCacheDownstream(t *testing.T) {
+	src, err := NewInternal()
+	require.NoError(t, err, "no error creating internal")
+
+	tgt, err := NewInternal()
+	require.NoError(t, err, "no error creating another internal")
+
+	cch := NewCacher(src, tgt, 24*time.Hour)
+
+	err = src.SetSecret(
+		&Secret{
+			Name:  "upstream",
+			Value: "secret",
+		},
+	)
+	require.NoError(t, err, "no error setting on source")
+
+	s, err := cch.GetSecret("upstream")
+	require.NoError(t, err, "no error getting on cacher")
+
+	assert.Equal(t, "upstream", s.Name, "got upstream secret name")
+	assert.Equal(t, "secret", s.Value, "got upstream secret value")
+
+	s2, err := tgt.GetSecret("upstream")
+	require.NoError(t, err, "no error getting on target")
+
+	assert.Equal(t, "upstream", s2.Name, "got upstream secret name copied to target")
+	assert.Equal(t, "secret", s2.Value, "got upstream secret value copied to target")
 }
