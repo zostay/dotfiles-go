42ae89eba86a92b185142b5dfb66c17bdd4f5c92
Sterling Hanenkamp <sterling@hanenkamp.com>
2021-05-21 09:35:01 -0500

    fix my own weirdness in this code
    Was I asleep when I wrote this? I don't get it.

--- cmd/zostay-secret/secrets.go
+++ cmd/zostay-secret/secrets.go
@@ -7,6 +7,27 @@ import (
 	"github.com/zostay/dotfiles-go/pkg/secrets"
 )
 
+// secretKeeper sets up all the secrets SDK, interprets the command-line
+// options, chooses or builds a secret keeper based on those options, and
+// returns a secret keeper.
+//
+// The behavior is as follows:
+//
+// If more than one secret keeper selection option is set
+// (--master/--local-only/--remove-only), then an error is returned.
+//
+// Otherwise, if --master is set, then this returns
+//
+//  secrets.Master()
+//
+// If --remote-only is set, this returns either secrets.SecureMain() or
+// secrets.InsecureMain() based on the --insecure option.
+//
+// If --local-only is set, this returns either secrets.SecureLocal() or
+// secrets.InsecureLocal() based on the --insecure option.
+//
+// Finally, if none of secret keeper selection options are set, it returns
+// either secrets.Secure() or secrets.Insecure() based on --insecure option.
 func secretKeeper() (secrets.Keeper, error) {
 	keeper.RequiresSecretKeeper()
 
@@ -21,42 +42,39 @@ func secretKeeper() (secrets.Keeper, error) {
 		if err != nil {
 			panic(err)
 		}
+	} else if remoteOnly {
+		var err error
+		if insecure {
+			k, err = secrets.InsecureMain()
+		} else {
+			k, err = secrets.SecureMain()
+		}
+
+		if err != nil {
+			panic(err)
+		}
+	} else if localOnly {
+		var err error
+		if insecure {
+			k, err = secrets.InsecureLocal()
+		} else {
+			k, err = secrets.SecureLocal()
+		}
+
+		if err != nil {
+			panic(err)
+		}
 	} else {
-		lt := secrets.NewLocumTenens()
-
-		if !localOnly && !masterOnly {
-			var kp secrets.Keeper
-			var err error
-			if insecure {
-				kp, err = secrets.InsecureLocal()
-			} else {
-				kp, err = secrets.SecureLocal()
-			}
-
-			if err != nil {
-				panic(err)
-			}
-
-			lt.AddKeeper(kp)
+		var err error
+		if insecure {
+			k, err = secrets.Insecure()
+		} else {
+			k, err = secrets.Secure()
 		}
 
-		if !remoteOnly && !masterOnly {
-			var lp secrets.Keeper
-			var err error
-			if insecure {
-				lp, err = secrets.InsecureMain()
-			} else {
-				lp, err = secrets.SecureMain()
-			}
-
-			if err != nil {
-				panic(err)
-			}
-
-			lt.AddKeeper(lp)
+		if err != nil {
+			panic(err)
 		}
-
-		k = lt
 	}
 
 	return k, nil
