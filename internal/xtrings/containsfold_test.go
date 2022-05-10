15b98da64febb1b9cba511e91087da83eea4f1ad
Sterling Hanenkamp <sterling@hanenkamp.com>
2022-05-10 11:10:17 -0500

    add test for ContainsFold

---
+++ internal/xtrings/containsfold_test.go
@@ -0,0 +1,37 @@
+package xtrings
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContainsFold(t *testing.T) {
+	var data = []struct {
+		s      string
+		substr string
+		pass   bool
+	}{
+		{"abc", "abc", true},
+		{"asdfabcasdf", "abc", true},
+		{"asdfasdf", "abc", false},
+		{"ABC", "abc", true},
+		{"asdfABCasdf", "abc", true},
+		{"abc", "ABC", true},
+		{"asdfabcasfd", "ABC", true},
+		{"ASDFabcASDF", "ABC", true},
+		{"ASDFABCASDF", "ABC", true},
+		{"abc", "ABCabc", false},
+	}
+
+	for _, test := range data {
+		assert.Equalf(
+			t,
+			test.pass,
+			ContainsFold(test.s, test.substr),
+			"%q contains %q",
+			test.s,
+			test.substr,
+		)
+	}
+}
