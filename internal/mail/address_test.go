3cc3ddf57e9b0cb172709e06ba7fd8e44590707d
Sterling Hanenkamp <sterling@hanenkamp.com>
2022-11-22 13:19:16 -0600

    adding a test

---
+++ internal/mail/address_test.go
@@ -0,0 +1,31 @@
+package mail
+
+import (
+	_ "net/http/pprof"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/zostay/go-addr/pkg/addr"
+)
+
+func TestAddressListStrings(t *testing.T) {
+	addrs, err := addr.ParseEmailAddressList("sterling@example.com, Foo@example.com, blahblah@example.com")
+	require.NoError(t, err)
+
+	strs := AddressListStrings(addrs)
+
+	assert.Equal(t, []string{
+		"sterling@example.com",
+		"Foo@example.com",
+		"blahblah@example.com",
+	}, strs)
+}
+
+func TestAddressListHTML(t *testing.T) {
+	addrs, err := addr.ParseEmailAddressList("sterling@example.com, Foo@example.com, blahblah@example.com")
+	require.NoError(t, err)
+
+	str := AddressListHTML(addrs)
+	assert.Equal(t, "<strong></strong> &lt;<a href=\"mailto:sterling@example.com\">sterling@example.com</a>&gt;, <strong></strong> &lt;<a href=\"mailto:Foo@example.com\">Foo@example.com</a>&gt;, <strong></strong> &lt;<a href=\"mailto:blahblah@example.com\">blahblah@example.com</a>&gt;", str)
+}
