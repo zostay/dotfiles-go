880d2819816823ad65632ed60fd4c831b6b926f9
Sterling Hanenkamp <sterling@speakeasyapi.dev>
2023-07-22 22:49:57 -0500

    adding seq from what was locum

--- pkg/secrets/v2/secret.go
+++ pkg/secrets/v2/secret.go
@@ -5,51 +5,89 @@ import (
 	"time"
 )
 
-// SingleOption is a function that can be used to modify a secret.
-type SingleOption func(*Single)
+// SingleOption is used to customize a secret during construction.
+type SingleOption interface {
+	apply(*Single)
+}
+
+type option func(*Single)
+
+func (o option) apply(s *Single) { o(s) }
 
 // WithType sets the type of the secret.
 func WithType(typ string) SingleOption {
-	return func(s *Single) {
+	return option(func(s *Single) {
 		s.typ = typ
-	}
+	})
 }
 
 // WithLastModified sets the last modified time for the secret.
 func WithLastModified(t time.Time) SingleOption {
-	return func(s *Single) {
+	return option(func(s *Single) {
 		s.lastModified = t
-	}
+	})
 }
 
 // WithUrl sets the URL for the secret.
 func WithUrl(u *url.URL) SingleOption {
-	return func(s *Single) {
+	return option(func(s *Single) {
 		s.url = u
-	}
+	})
 }
 
 // WithLocation sets the location for the secret.
 func WithLocation(l string) SingleOption {
-	return func(s *Single) {
+	return option(func(s *Single) {
 		s.location = l
-	}
+	})
 }
 
 // WithField sets a field on the secret.
 func WithField(name, value string) SingleOption {
-	return func(s *Single) {
+	return option(func(s *Single) {
 		s.fields[name] = value
-	}
+	})
 }
 
 // WithFields sets the given fields on the secret.
 func WithFields(fields map[string]string) SingleOption {
-	return func(s *Single) {
+	return option(func(s *Single) {
 		for name, value := range fields {
 			s.fields[name] = value
 		}
-	}
+	})
+}
+
+// WithID sets the ID of the secret, which is useful when copying a secret using
+// NewSingleFromSecret.
+func WithID(id string) SingleOption {
+	return option(func(s *Single) {
+		s.id = id
+	})
+}
+
+// WithName sets the name of the secret for use when copying a secret using
+// NewSingleFromSecret.
+func WithName(name string) SingleOption {
+	return option(func(s *Single) {
+		s.name = name
+	})
+}
+
+// WithUsername sets the username of the secret for use when copying a secret
+// using NewSingleFromSecret.
+func WithUsername(username string) SingleOption {
+	return option(func(s *Single) {
+		s.username = username
+	})
+}
+
+// WithSecret sets the password of the secret for use when copying a secret
+// using NewSingleFromSecret.
+func WithSecret(secret string) SingleOption {
+	return option(func(s *Single) {
+		s.secret = secret
+	})
 }
 
 // Single represents a single secret stored in a Keeper.
@@ -78,15 +116,16 @@ func NewSecret(id, name, username, secret string, opts ...SingleOption) *Single 
 	}
 
 	for _, opt := range opts {
-		opt(sec)
+		opt.apply(sec)
 	}
 
 	return sec
 }
 
-// NewSecretFromSecret creates a *Single from the given secret.
-func NewSingleFromSecret(s Secret) *Single {
-	return &Single{
+// NewSecretFromSecret creates a *Single from the given secret with the
+// requested modifications applied.
+func NewSingleFromSecret(s Secret, opts ...SingleOption) *Single {
+	sec := &Single{
 		id:       s.ID(),
 		name:     s.Name(),
 		username: s.Username(),
@@ -99,6 +138,12 @@ func NewSingleFromSecret(s Secret) *Single {
 		url:          s.Url(),
 		location:     s.Location(),
 	}
+
+	for _, opt := range opts {
+		opt.apply(sec)
+	}
+
+	return sec
 }
 
 // ID returns the unique identifier for the secret.
